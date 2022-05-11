1c9490054e1977b84ebf23bafd5cb63a94c66968
zyw-200 <7752060+zyw-200@users.noreply.github.com>
2022-05-11 19:07:22 +0800

    update

--- procinfo_generic/.procinfo.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yaowen/AGL_setup/procinfo_generic/procinfo.mod := { echo  /home/yaowen/AGL_setup/procinfo_generic/procinfo.o;  echo; } > /home/yaowen/AGL_setup/procinfo_generic/procinfo.mod
